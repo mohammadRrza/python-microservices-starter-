ee3a025dc881bb11da15ef2cdca89573e4c4ae43
Reza <m.taheri25461@gmail.com>
2026-04-11 17:11:32 +0200

    run user service

--- services/user-service/app/main.py
+++ services/user-service/app/main.py
@@ -1,23 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.db import Base, engine, get_db
+from app.models import User
+from app.schemas import UserCreate, UserResponse
 
 app = FastAPI(title="User Service")
 
-fake_users = [
-    {"id": 1, "name": "Ali", "email": "ali@example.com"},
-    {"id": 2, "name": "Sara", "email": "sara@example.com"},
-]
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/health")
 def health():
     return {"status": "ok", "service": "user-service"}
 
-@app.get("/users")
-def get_users():
-    return fake_users
 
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    for user in fake_users:
-        if user["id"] == user_id:
-            return user
-    return {"message": "User not found"}
+@app.post("/users", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already exists")
+
+    new_user = User(name=user.name, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/users", response_model=list[UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
+
+
+@app.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@app.get("/")
+def root():
+    return {"message": "Gateway is running"}
